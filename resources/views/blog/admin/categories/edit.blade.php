c0ec75d1e017e285875cfc24bcce88dd9beabdcb
ALexus <vorgen.morgen@yandex.ru>
2021-08-26 14:24:19 +0200

    Laravel #23/DmitriyAfanasyev

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -23,7 +23,12 @@
             @if(session('success'))
                 <div class="row justify-content-center">
                     <div class="col-md-11">
-                        <div class="alert alert-success" role="alert">Успешно сохранено!</div>
+                        <div class="alert alert-success" role="alert">
+                            <button class="close" type="button" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">x</span>
+                            </button>
+                            {{ session()->get('success') }}
+                        </div>
                     </div>
                 </div>
             @endif
